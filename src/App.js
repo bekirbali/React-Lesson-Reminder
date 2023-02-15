51eadc9cd6faa2e72e7cf25cc8863ecdc8981e2c
bekirbali <bfbali43@gmail.com>
2023-02-15 13:00:21 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Lesson from "./components/Lesson";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Lesson />
+      </div>
+    </div>
+  );
 }
 
 export default App;
